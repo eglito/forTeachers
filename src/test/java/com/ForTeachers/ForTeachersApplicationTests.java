9e301595b8ef47508870404f64d07dc6bea75bc6
eglito <aleeglito@gmail.com>
2025-04-21 22:23:40 -0300

    feat (ForTeachersApplication): criar classe de inicialização.

---
+++ src/test/java/com/ForTeachers/ForTeachersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ForTeachers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForTeachersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
