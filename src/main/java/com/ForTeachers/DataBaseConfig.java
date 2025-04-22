0427bacb8300455ac74691c1026ed4cfb2559b6d
eglito <aleeglito@gmail.com>
2025-04-21 22:29:54 -0300

    feat (DataBaseConfig): classe de configuração do banco de dados.

---
+++ src/main/java/com/ForTeachers/DataBaseConfig.java
@@ -0,0 +1,26 @@
+package com.ForTeachers;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DataBaseConfig {
+
+    private static HikariConfig config = new HikariConfig ();
+    private static HikariDataSource ds;
+
+    static {
+        config.setJdbcUrl ("jdbc_url");
+        config.setUsername ("forteachers");
+        config.setPassword ("1234");
+        ds = new HikariDataSource (config);
+    }
+
+    private void DataSource() {}
+
+    public static Connection getConnection() throws SQLException{
+        return ds.getConnection ();
+    }
+}
