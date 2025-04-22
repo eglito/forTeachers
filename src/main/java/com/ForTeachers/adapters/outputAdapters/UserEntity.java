9f331ee542538d7b012e1f94a3f464f0fc64f756
eglito <aleeglito@gmail.com>
2025-04-21 22:28:19 -0300

    feat (student): modificar entidade para tipos de usuários.

---
+++ src/main/java/com/ForTeachers/adapters/outputAdapters/UserEntity.java
@@ -0,0 +1,33 @@
+package com.ForTeachers.adapters.outputAdapters;
+
+import com.ForTeachers.enums.StudentType;
+import com.ForTeachers.enums.UserType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table (name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Inheritance(strategy = InheritanceType.JOINED)
+public abstract class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 150)
+    private String firstName;
+    @Column(length = 150)
+    private String lastName;
+    @Column(unique = true)
+    private String email;
+    @Column(name = "user_type")
+    private UserType userType;
+    private String password;
+
+}
