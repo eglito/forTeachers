3d8b59c52598b90493467b54f99ebe41756f1857
eglito <aleeglito@gmail.com>
2025-04-26 23:02:32 -0300

    feat (TeacherRepository): implementar a interface para a persistência da entidade TeacherEntity

---
+++ src/main/java/com/ForTeachers/repositorios/TeacherRepository.java
@@ -0,0 +1,10 @@
+package com.ForTeachers.repositorios;
+
+import com.ForTeachers.adapters.outputAdapters.TeacherEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TeacherRepository extends JpaRepository<TeacherEntity, Long> {
+
+}
