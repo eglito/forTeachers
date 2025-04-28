28e60bea864227f4f12e74600e1dec5b4cb89316
eglito <aleeglito@gmail.com>
2025-04-28 18:30:42 -0300

    feat (StudentRepository): implementar anotação ao repositório

--- src/main/java/com/ForTeachers/repositorios/StudentRepository.java
+++ src/main/java/com/ForTeachers/repositorios/StudentRepository.java
@@ -2,7 +2,9 @@ package com.ForTeachers.repositorios;
 
 import com.ForTeachers.adapters.outputAdapters.StudentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StudentRepository extends JpaRepository<StudentEntity, Long> {
 
 }
