824ce43917718039a443c11c1e50027fd999ac9c
eglito <aleeglito@gmail.com>
2025-05-16 08:26:05 -0300

    feat (LessonMapper): implementar a classe LessonMapper, que converte LessonRequestDTO para LessonEntity; converte LessonEntity para LessonRequestDTO

---
+++ src/main/java/com/ForTeachers/mapper/LessonMapper.java
@@ -0,0 +1,31 @@
+package com.ForTeachers.mapper;
+
+import com.ForTeachers.adapters.outputAdapters.LessonEntity;
+import com.ForTeachers.dtos.lessonDto.LessonRequestDTO;
+import com.ForTeachers.dtos.lessonDto.LessonResponseDTO;
+
+public class LessonMapper {
+
+    public LessonMapper() {
+
+    }
+
+    public LessonEntity toEntity(LessonRequestDTO dto){
+
+        LessonEntity entity = new LessonEntity ();
+        entity.setLessonTitle (dto.lessonTitle ());
+
+        return entity;
+    }
+
+    public LessonResponseDTO toResponse (LessonEntity entity){
+
+        LessonResponseDTO responseDTO = new LessonResponseDTO(
+                entity.getLessonTitle ()
+        );
+
+        return responseDTO;
+
+    }
+
+}
