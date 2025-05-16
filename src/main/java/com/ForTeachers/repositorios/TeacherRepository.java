6c5d117aba06b6ec79ceb000da618272cf49e568
eglito <aleeglito@gmail.com>
2025-05-16 08:40:40 -0300

    feat (TeacherRepository): refatorar TeacherRepository.

--- src/main/java/com/ForTeachers/repositorios/TeacherRepository.java
+++ src/main/java/com/ForTeachers/repositorios/TeacherRepository.java
@@ -2,6 +2,7 @@ package com.ForTeachers.repositorios;
 
 import com.ForTeachers.adapters.outputAdapters.TeacherEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import org.springframework.stereotype.Repository;
 
 @Repository
