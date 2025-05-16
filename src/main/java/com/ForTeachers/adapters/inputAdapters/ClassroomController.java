0ec01c2e5637c3f3a3f760556783a1270d7f9190
eglito <aleeglito@gmail.com>
2025-05-16 08:18:49 -0300

    feat (StudentEntity): implementar o controller de classroom

---
+++ src/main/java/com/ForTeachers/adapters/inputAdapters/ClassroomController.java
@@ -0,0 +1,30 @@
+package com.ForTeachers.adapters.inputAdapters;
+
+import com.ForTeachers.dtos.classroomDTO.ClassroomRequestDTO;
+import com.ForTeachers.dtos.classroomDTO.ClassroomResponseDTO;
+import com.ForTeachers.services.ClassroomService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/classroom")
+public class ClassroomController {
+
+    @Autowired
+    private ClassroomService classroomService;
+
+    @PostMapping("/{id}")
+    public ResponseEntity<ClassroomResponseDTO> save(@RequestBody ClassroomRequestDTO requestDTO, @PathVariable Long id){
+        return ResponseEntity.ok (classroomService.save (requestDTO, id));
+    }
+
+    @DeleteMapping("{teacherId}/{classroomId}")
+    public void delete(@PathVariable Long teacherId,@PathVariable Long classroomId){
+        classroomService.delete (teacherId, classroomId);
+    }
+}
