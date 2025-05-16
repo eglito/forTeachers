55251ab44c5aaad63d643dec04a47e2866ab6a23
eglito <aleeglito@gmail.com>
2025-05-16 08:42:12 -0300

    feat (ClassroomResponseDTO): implementar record DTO para resposta de classroom.

---
+++ src/main/java/com/ForTeachers/dtos/classroomDTO/ClassroomResponseDTO.java
@@ -0,0 +1,9 @@
+package com.ForTeachers.dtos.classroomDTO;
+
+public record ClassroomResponseDTO(
+        String name,
+        String firstName,
+        String lastName,
+        String email
+) {
+}
