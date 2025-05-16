ce9bfcf05ad06a4beb02214811df94ee955d6cb7
eglito <aleeglito@gmail.com>
2025-05-16 08:31:11 -0300

    feat (TeacherEntity): refatorar a entidade TeacherEntity. Implementar os métodos para adicionar e remover Discipline, Classroom, Student e Lesson das listas.

--- src/main/java/com/ForTeachers/adapters/outputAdapters/TeacherEntity.java
+++ src/main/java/com/ForTeachers/adapters/outputAdapters/TeacherEntity.java
@@ -17,7 +17,7 @@ import java.util.List;
 public class TeacherEntity extends UserEntity{
 
     @OneToMany(mappedBy = "teacher", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    private List<StudentEntity> studentsList;
+    private List<StudentEntity> listStudent;
 
     @OneToMany(mappedBy = "teacher", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<DisciplineEntity> listDisciplines;
@@ -27,5 +27,46 @@ public class TeacherEntity extends UserEntity{
 
     @OneToMany(mappedBy = "teacher", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<LessonEntity> listLesson;
+
+
+    public void addStudent(StudentEntity student){
+        listStudent.add(student);
+        student.setTeacher (this);
+    }
+
+    public void removeStudent(StudentEntity student){
+        listStudent.remove (student);
+        student.setTeacher(null);
+    }
+
+    public void addDisciplines(DisciplineEntity discipline){
+        listDisciplines.add (discipline);
+        discipline.setTeacher (this);
+    }
+
+    public void removeDisciplines (DisciplineEntity discipline){
+        listDisciplines.remove (discipline);
+        discipline.setTeacher(null);
+    }
+
+    public void addClassroom(ClassroomEntity classroom){
+        listClassroom.add (classroom);
+        classroom.setTeacher (this);
+    }
+
+    public void removeClassroom(ClassroomEntity classroom){
+        listClassroom.remove (classroom);
+        classroom.setTeacher (null);
+    }
+
+    public void addLesson(LessonEntity lesson){
+        listLesson.add (lesson);
+        lesson.setTeacher (this);
+    }
+
+    public void removeLesson(LessonEntity lesson){
+        listLesson.remove (lesson);
+        lesson.setTeacher (null);
+    }
 
 }
