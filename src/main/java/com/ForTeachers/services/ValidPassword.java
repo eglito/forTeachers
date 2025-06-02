fe62cf411cec05d1993debdb77112803557ea666
eglito <aleeglito@gmail.com>
2025-06-01 22:49:54 -0300

    feat (ValidPassword): classe que valida senhas via regex.

---
+++ src/main/java/com/ForTeachers/services/ValidPassword.java
@@ -0,0 +1,27 @@
+package com.forteachers.services;
+
+
+import org.springframework.stereotype.Service;
+
+import java.util.regex.Pattern;
+
+@Service
+public class ValidPassword {
+
+    public String password;
+
+    public ValidPassword(){
+        this.password = password;
+    }
+
+    public Boolean validPassoword(String password) throws IllegalAccessException {
+
+        this.password = password;
+
+        String regex = "^(?=.*\\d).{8,}$";
+        if(Pattern.matches(regex, this.password)){
+            return true;
+        } throw new IllegalAccessException("A senha deve ter entre 8 e 30 caracteres e conter " +
+                "uma letra maiúscula, um símbolo e um número");
+    }
+}
