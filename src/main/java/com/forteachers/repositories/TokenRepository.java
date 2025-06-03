dca539a16240236d327db142913912d3c246f6ce
AlexandreLuizAdriano <aleeglito@gmail.com>
2025-06-03 09:32:11 -0300

    refactor (TokenRepository): adicionar método findByToken.

--- src/main/java/com/forteachers/repositories/TokenRepository.java
+++ src/main/java/com/forteachers/repositories/TokenRepository.java
@@ -3,5 +3,9 @@ package com.forteachers.repositories;
 import com.forteachers.InvitationToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TokenRepository extends JpaRepository<InvitationToken,Long> {
+
+    Optional<InvitationToken> findByToken(String token);
 }
