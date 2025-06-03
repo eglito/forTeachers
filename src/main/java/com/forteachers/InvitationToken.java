a60f0b16547979e0c4bf6222f8718f18a5fa843a
AlexandreLuizAdriano <aleeglito@gmail.com>
2025-06-03 09:35:08 -0300

    refactor (InvitationToken): adicionar anotações lombok e motificar tipo de expiryDate. Remover campos e métodos devido à nova abordagem para geração de token.

--- src/main/java/com/forteachers/InvitationToken.java
+++ src/main/java/com/forteachers/InvitationToken.java
@@ -3,12 +3,18 @@ package com.forteachers;
 import com.forteachers.adapters.outputAdapters.ClassroomEntity;
 import com.forteachers.adapters.outputAdapters.TeacherEntity;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import java.sql.Timestamp;
-import java.util.Calendar;
-import java.util.Date;
+import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "invitationToken")
 public class InvitationToken {
     private static final int EXPIRATION = 60 * 24;
@@ -23,71 +29,10 @@ public class InvitationToken {
     @JoinColumn(nullable = false, name = "teacher_id")
     private TeacherEntity teacher;
 
-    public ClassroomEntity getClassroom() {
-        return classroom;
-    }
-
-    public void setClassroom(ClassroomEntity classroom) {
-        this.classroom = classroom;
-    }
-
-    public TeacherEntity getTeacher() {
-        return teacher;
-    }
-
     @ManyToOne(targetEntity = ClassroomEntity.class, fetch = FetchType.LAZY)
     @JoinColumn(nullable = false,name = "classroom_id")
     private ClassroomEntity classroom;
 
+    private LocalDateTime expiryDate;
 
-    private Date expiryDate;
-
-    public InvitationToken(String token, TeacherEntity teacher, Date expiryDate) {
-        this.token = token;
-        this.teacher = teacher;
-        this.expiryDate = expiryDate;
-    }
-
-    public InvitationToken(){
-
-    }
-
-    public Date calculateExpiryDate(int expiryTimeInMinutes){
-        Calendar cal = Calendar.getInstance ();
-        cal.setTime (new Timestamp (cal.getTime().getTime ()));
-        cal.add (Calendar.MINUTE, expiryTimeInMinutes);
-        return new Date(cal.getTime ().getTime ());
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public TeacherEntity getUser() {
-        return teacher;
-    }
-
-    public void setTeacher(TeacherEntity teacher) {
-        this.teacher = teacher;
-    }
-
-    public Date getExpiryDate() {
-        return expiryDate;
-    }
-
-    public void setExpiryDate(Date expiryDate) {
-        this.expiryDate = expiryDate;
-    }
 }
