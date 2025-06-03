890a6999f1108e4a981ef9dd9969b84ff5d2b799
AlexandreLuizAdriano <aleeglito@gmail.com>
2025-06-03 09:30:28 -0300

    feat (TeacherService): adicioar método generateInvitationToken para o token de criação de usuários do tipo STUDENT.

--- src/main/java/com/forteachers/services/TeacherService.java
+++ src/main/java/com/forteachers/services/TeacherService.java
@@ -1,5 +1,7 @@
 package com.forteachers.services;
 
+import com.forteachers.InvitationToken;
+import com.forteachers.adapters.outputAdapters.ClassroomEntity;
 import com.forteachers.mapper.TeacherMapper;
 import com.forteachers.persistence.TeacherPersistence;
 import com.forteachers.adapters.outputAdapters.TeacherEntity;
@@ -7,11 +9,15 @@ import com.forteachers.dtos.EmailResponseDTO;
 import com.forteachers.dtos.userDto.UserResponseDTO;
 import com.forteachers.dtos.userDto.UserRequestDTO;
 import com.forteachers.enums.UserType;
+import com.forteachers.repositories.ClassroomRepository;
 import com.forteachers.repositories.TeacherRepository;
+import com.forteachers.repositories.TokenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class TeacherService implements TeacherPersistence {
@@ -19,10 +25,14 @@ public class TeacherService implements TeacherPersistence {
     @Autowired
     private final TeacherRepository teacherRepository;
     private PasswordBcrypt passwordBcrypt;
+    private ClassroomRepository classroomRepository;
+    private TokenRepository tokenRepository;
 
-    public TeacherService(TeacherRepository teacherRepository, PasswordBcrypt passwordBcrypt) {
+    public TeacherService(TeacherRepository teacherRepository, PasswordBcrypt passwordBcrypt, ClassroomRepository classroomRepository, TokenRepository tokenRepository) {
         this.teacherRepository = teacherRepository;
         this.passwordBcrypt = passwordBcrypt;
+        this.classroomRepository = classroomRepository;
+        this.tokenRepository = tokenRepository;
     }
 
 
@@ -95,4 +105,23 @@ public class TeacherService implements TeacherPersistence {
         teacherRepository.deleteById(id);
     }
 
+    public String generateInvitationToken(Long teacherId, Long classroomId){
+
+        TeacherEntity teacher = teacherRepository.findById(teacherId)
+                .orElseThrow(() -> new RuntimeException("Usuário não encontrado!"));
+
+        ClassroomEntity classroom = classroomRepository.findById(classroomId)
+                .orElseThrow(() -> new RuntimeException("Sala de aula não encontrada"));
+
+        String token = UUID.randomUUID().toString();
+
+        InvitationToken invitationToken = new InvitationToken();
+        invitationToken.setToken(token);
+        invitationToken.setTeacher(teacher);
+        invitationToken.setClassroom(classroom);
+        invitationToken.setExpiryDate(LocalDateTime.now().plusDays(2));
+        tokenRepository.save(invitationToken);
+
+        return token;
+    }
 }
