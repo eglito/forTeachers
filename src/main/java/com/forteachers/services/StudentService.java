b8cf2b28da781244342b4bbf3e45f253a2c560dc
eglito <aleeglito@gmail.com>
2025-06-04 21:53:50 -0300

    refactor (StudentService): update registerStudents method to use StudentRequestDTO and TokenResponseDTO

--- src/main/java/com/forteachers/services/StudentService.java
+++ src/main/java/com/forteachers/services/StudentService.java
@@ -1,9 +1,11 @@
 package com.forteachers.services;
 
-import com.forteachers.InvitationToken;
+import com.forteachers.adapters.outputAdapters.InvitationToken;
 import com.forteachers.adapters.outputAdapters.StudentEntity;
-import com.forteachers.dtos.userDto.UserRequestDTO;
-import com.forteachers.dtos.userDto.UserResponseDTO;
+import com.forteachers.dtos.studentDto.StudentRequestDTO;
+import com.forteachers.dtos.studentDto.StudentResponseDTO;
+import com.forteachers.dtos.tokenDto.TokenResponseDTO;
+import com.forteachers.enums.UserType;
 import com.forteachers.mapper.StudentMapper;
 import com.forteachers.persistence.StudentPersistence;
 import com.forteachers.repositories.StudentRepository;
@@ -13,24 +15,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 public class StudentService implements StudentPersistence {
 
     @Autowired
     private StudentRepository studentRepository;
+    @Autowired
     private TeacherRepository teacherRepository;
+    @Autowired
     private TokenRepository tokenRepository;
+    @Autowired
     private PasswordBcrypt passwordBcrypt;
 
-    public StudentService(StudentRepository studentRepository, TeacherRepository teacherRepository, TokenRepository tokenRepository,PasswordBcrypt passwordBcrypt) {
-        this.studentRepository = studentRepository;
-        this.teacherRepository = teacherRepository;
-        this.tokenRepository = tokenRepository;
-        this.passwordBcrypt = passwordBcrypt;
-    }
-
-    public UserResponseDTO registerStudents(String token, UserRequestDTO userRequestDTO) throws IllegalAccessException {
+    public StudentResponseDTO registerStudents(String token, StudentRequestDTO studentRequestDTO) throws IllegalAccessException {
 
         InvitationToken invitationToken = tokenRepository.findByToken(token)
                 .orElseThrow(() -> new RuntimeException("Token inválido"));
@@ -38,11 +37,27 @@ public class StudentService implements StudentPersistence {
         if(invitationToken.getExpiryDate().isBefore(LocalDateTime.now())){
             throw new RuntimeException("Token expirado");
         }
+        if(!studentRequestDTO.userType ().equals (UserType.STUDENT)){
+            throw new RuntimeException ("Usuário deve ser do tipo STUDENT");
+        }
 
-        StudentEntity student = new StudentMapper().toEntity(userRequestDTO);
-        student.setPassword(passwordBcrypt.hashing(userRequestDTO.password()));
+        StudentEntity student = new StudentMapper().toEntity(studentRequestDTO);
+
+        TokenResponseDTO tokenInformation = tokenInformation (token);
+        student.setTeacher (tokenInformation.teacher ());
+        student.addClassroom (tokenInformation.classroom());
+
+        tokenInformation.teacher ().addStudent (student);
+
+        student.setPassword(passwordBcrypt.hashing(studentRequestDTO.password()));
+
         studentRepository.save(student);
 
         return new StudentMapper().toResponseDTO(student);
+    }
+
+    private TokenResponseDTO tokenInformation(String token){
+        Optional<InvitationToken> invitationToken = tokenRepository.findByToken (token);
+        return new TokenResponseDTO (invitationToken.get ().getTeacher (), invitationToken.get ().getClassroom ());
     }
 }
